afeb25240f6639eb684ac7be5fd26709b2d735d6
ParthG2809 <parthgoyal2809@gmail.com>
2026-02-04 12:51:20 +0530

    Module-2

---
+++ Assignments/Module-2/dynamic_list.py
@@ -0,0 +1,8 @@
+technologies = []
+elements = int(input("Enter the number of Elements of the list: "))
+
+for i in range(elements):
+    data = input("Enter the data: ", )
+    technologies.append(data)
+
+print(technologies)
